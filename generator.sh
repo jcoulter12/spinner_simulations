a66d3eb703c14c19d2129d35f8dcccb83e8b0fbb
jcoulter12 <jenny.coulter@rutgers.edu>
2016-07-14 15:00:08 -0400

    typo in generator

--- generator.sh
+++ generator.sh
@@ -13,22 +13,23 @@ do
 	  noise=$n
 	  n=$(($n*10))
       fi
-      export noise
+      #export noise
       w=1
       omega=1
       while [ $w -le 3 ]
       do
 	omega=$(($omega*10))
-	export omega
+	#export omega
       	echo "mkdir outputs/PDFs-N$noise-W$omega"
 	echo "cd outputs/PDFs-N$noise-W$omega"
 	i=0
+	eta=0
 	echo "nohup python2.7 /Users/aak/Desktop/spinner_simulations/spinner_sim.py $i $noise $eta $omega >> out.txt &"
 	let "i++"
 	while [ $i -le $nJobs ];
 	do
 	   jobNum=$i
-	   export jobNum
+	   #export jobNum
 	   eta=$(echo "scale=1;0.1+$i*0.4" | bc)
 	   #export eta
 	   echo "nohup python2.7 /Users/aak/Desktop/spinner_simulations/spinner_sim.py $i $noise $eta $omega >> out.txt &"
