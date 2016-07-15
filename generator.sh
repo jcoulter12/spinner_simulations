bd0aba31c4edf67e7a668fe6d825977f681b316a
jcoulter12 <jenny.coulter@rutgers.edu>
2016-07-15 01:17:43 -0400

    tiny changes

--- generator.sh
+++ generator.sh
@@ -31,7 +31,7 @@ do
 	   		#export jobNum
 	   		eta=$(echo "scale=1;0.1+$i*0.4" | bc)
 	  		#export eta
-	   		echo "nohup python2.7 /Users/aak/Desktop/spinner_simulations/spinner_sim.py $i $noise $eta $omega >> out.txt &"
+	   		echo "nohup python2.7 /Users/aak/Desktop/spinner_simulations/spinner_sim.py $jobNum $noise $eta $omega >> out.txt &"
 	   		let "i++"
 		done
 		echo "cd ../../"
